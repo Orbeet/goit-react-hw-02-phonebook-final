e837bcb3517f7be61c5a57787596fe88a0d3dac1
orbeet <excenter8@gmail.com>
2020-08-17 16:13:44 +0300

    phone

---
+++ src/components/App/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import styles from "./App.module.css";
+
+import Section from "../Section/Section";
+import Notification from "../Notification/Notification";
+import ContactsList from "../ContactsList/ContactsList";
+import Filter from "../Filter/Filter";
+import CreateContactForm from "../CreateContactForm/CreateContactForm";
+
+const filterContacts = (contacts, filter) => {
+  return contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+};
+
+export default class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  changeFilter = (event) => {
+    this.setState({ filter: event.target.value });
+  };
+
+  addContact = (contact) => {
+    const isUniqueName = this.state.contacts.some(
+      (savedContact) =>
+        savedContact.name.toLowerCase() === contact.name.toLowerCase()
+    );
+
+    if (isUniqueName) {
+      return alert(`${contact.name} is already in contacts`);
+    }
+    const contactToAdd = {
+      ...contact,
+      id: uuidv4(),
+    };
+    this.setState((state) => ({
+      contacts: [...state.contacts, contactToAdd],
+    }));
+
+    return null;
+  };
+
+  deleteContact = (id) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((contact) => contact.id !== id),
+      filter: "",
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = filterContacts(contacts, filter);
+
+    return (
+      <div className={styles.container}>
+        <h1>goit-react-hw-02-phonebook</h1>
+        <Section title="Phonebook">
+          <CreateContactForm onAddContact={this.addContact} />
+        </Section>
+        <Section title="Contacts">
+          {contacts.length >= 2 && (
+            <Filter value={filter} onChangeFilter={this.changeFilter} />
+          )}
+          {filteredContacts.length > 0 ? (
+            <ContactsList
+              contacts={filteredContacts}
+              onDeleteContact={this.deleteContact}
+            />
+          ) : (
+            <Notification message="Contacts for query not found" />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
